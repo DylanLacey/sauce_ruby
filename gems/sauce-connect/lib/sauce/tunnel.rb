083a14fe2b8b83cd7f3a2e16e716e96c376a2b5f
Dylan Lacey <github@dylanlacey.com>
2013-06-25 23:51:33 -0700

    Reworked tunnels to not use the block form -- unreliable.

--- gems/sauce-connect/lib/sauce/tunnel.rb
+++ gems/sauce-connect/lib/sauce/tunnel.rb
@@ -7,6 +7,8 @@ module Sauce
 
       attr_reader :tunnel, :running
 
+      MAX_STRING = 500
+
       STRING_METHODS = [
           :tunnel_host,
           :proxy_host,
@@ -86,31 +88,73 @@ module Sauce
         set_integer_parameter :log_level, LOG_LEVELS[value]
       end
 
-      def set_integer_parameter(parameter, value)
-        FFI::MemoryPointer.new :int do |p|
-          p.write_array_of_int [value]
-          sc_set tunnel, PARAMETERS[parameter] , p
+      def log_level
+        unparsed_level = get_integer_parameter :log_level
+        return LOG_LEVELS.select {|k,v| v == unparsed_level}.first[0]
+      end
+
+      def get_integer_parameter(parameter)
+        FFI::MemoryPointer.new(:int, 1) do |ptr|
+          fetch_success = sc_get tunnel, PARAMETERS[parameter], ptr, ptr.type_size
+
+          unless fetch_success == 0
+            raise StandardError "Unable to get #{parameter} from Sauce Connect tunnel #{tunnel.object_id}"
+          end
+
+          return ptr.get_int(0)
         end
       end
 
-      def set_string_parameter(parameter, value)
-        FFI::MemoryPointer.from_string(value) do |p|
-          sc_set tunnel, PARAMETERS[parameter], p
+      def get_string_parameter(parameter)
+        FFI::MemoryPointer.new(:string, MAX_STRING) do |ptr|
+          fetch_success = sc_get tunnel, PARAMETERS[parameter], ptr, MAX_STRING
+
+          unless fetch_success == 0
+            raise StandardError "Unable to get #{parameter} from Sauce Connect tunnel #{tunnel.object_id}"
+          end
+
+          return ptr.get_string(0)
         end
+      end
+
+      def set_integer_parameter(parameter, value)
+        param = FFI::MemoryPointer.new(:int, 1)
+        param.write_array_of_int([value])
+        sc_set tunnel, PARAMETERS[parameter], param
+      end
+
+      def set_string_parameter(parameter, value)
+        param = FFI::MemoryPointer.from_string(value)
+        sc_set tunnel, PARAMETERS[parameter], param
       end
 
       def method_missing(method, *args)
         if method.to_s.end_with? "="
-          stripped_method = method.to_s.chop.to_sym
-          if STRING_METHODS.include? stripped_method
-            set_string_parameter(stripped_method, args[0])
-          elsif INTEGER_METHODS.include? stripped_method
-            set_integer_parameter(stripped_method, args[0])
-          else
-            super
-          end
+          return_value = attempt_missing_setter(method, args)
         else
-          super
+          return_value = attempt_missing_getter(method)
+        end
+
+        return return_value unless return_value.nil?
+        super
+      end
+
+      private
+
+      def attempt_missing_getter(method)
+        if STRING_METHODS.include? method
+          get_string_parameter method
+        elsif INTEGER_METHODS.include? method
+          get_integer_parameter method
+        end
+      end
+
+      def attempt_missing_setter(method, args)
+        stripped_method = method.to_s.chop.to_sym
+        if STRING_METHODS.include? stripped_method
+          set_string_parameter(stripped_method, args[0])
+        elsif INTEGER_METHODS.include? stripped_method
+          set_integer_parameter(stripped_method, args[0])
         end
       end
     end
