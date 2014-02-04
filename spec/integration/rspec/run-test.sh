908cca8c816f12fdbfc92146f12b3c7d6f04d23d
Dylan Lacey <github@dylanlacey.com>
2014-02-04 04:09:53 -0800

    Adding bundler

--- spec/integration/rspec/run-test.sh
+++ spec/integration/rspec/run-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash --login
 RAKE="bundle exec rake"
 rvm current
-rvm use 1.9.3-p484@sauce_ruby --create
+#rvm use 1.9.3-p484@sauce_ruby --create
+gem install bundler
 bundle install
 
 ${RAKE} spec
